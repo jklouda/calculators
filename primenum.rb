f91c5889436154443ea3626979f822c297efb34d
jklouda <jklouda@users.noreply.github.com>
2014-09-09 07:57:23 -0400

    Delete primenum.rb

--- primenum.rb
+++
@@ -1,7 +0,0 @@
-puts "This calculator will determine the nth prime number after 1.  Please enter which prime number after 1 you would like to find (1 = 1st, 3 = 3rd, 8 = 8th etc.)"
-gets = nth_prime
-
-remainder = 1
-
-while remainder =! 1
-  
